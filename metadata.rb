2f0d12d8f959b5f3993841ac554e433633989ece
Ian Smith <ian.smith@pnnl.gov>
2015-07-21 11:42:47 -0700

    updating metadata version and adding cookbook name

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
-maintainer       "Kyle Bader"
-maintainer_email "kyle.bader@dreamhost.com"
-license          "Apache 2.0"
-description      "Installs/Configures pdsh"
+name             'pdsh'
+maintainer       'Kyle Bader'
+maintainer_email 'kyle.bader@dreamhost.com'
+license          'Apache 2.0'
+description      'Installs/Configures pdsh'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          '0.1.2'
